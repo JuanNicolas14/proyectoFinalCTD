86188c043a901eac89f182cb4c6389a72d633b07
JuanNicolas14 <juan.cardenasc@outlook.com>
2023-05-25 12:45:55 -0500

    correciones e implementaciones, se arreglo un bug que ocurria al intentar agregar un restaurante repetido, ahora muestra correctamente su mensaje de error, al igual que al agregar y eliminar producto se muestran mensajes de confirmacion

--- Frontend/src/Componentes/Carrousel/Carrousel.jsx
+++ Frontend/src/Componentes/Carrousel/Carrousel.jsx
@@ -15,7 +15,7 @@ const slideStyles = {
     transform: "translate(0, -50%)",
     right: "32px",
     fontSize: "45px",
-    color: "black",
+    color: "#D4DADD",
     zIndex: 1,
     cursor: "pointer",
   };
@@ -26,7 +26,7 @@ const slideStyles = {
     transform: "translate(0, -50%)",
     left: "32px",
     fontSize: "45px",
-    color: "black",
+    color: "#D4DADD",
     zIndex: 1,
     cursor: "pointer",
   };
@@ -58,19 +58,24 @@ const Carrousel = ({imagen}) => {
       ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
+
     const goToPrevious = () => {
         const isFirstSlide = currentIndex === 0;
         const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
         setCurrentIndex(newIndex);
     };
+
     const goToNext = () => {
         const isLastSlide = currentIndex === slides.length - 1;
         const newIndex = isLastSlide ? 0 : currentIndex + 1;
         setCurrentIndex(newIndex);
     };
+
     const goToSlide = (slideIndex) => {
         setCurrentIndex(slideIndex);
     };
+
+
     const slideStylesWidthBackground = {
         ...slideStyles,
         backgroundImage: `url(${slides[currentIndex].url})`,
