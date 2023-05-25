581956af952e1bd00bd2816c15af2b51c2fa92e0
CesarMauricioRojas <mauricio2328cr@gmail.com>
2023-05-25 15:54:57 -0500

    Historia de usuario 10, implementación dependencia JWT, creación carpeta seguridad con JwtFilter, JwUtil y SecurityConfiguration

---
+++ Backend/src/main/java/com/example/Backend/security/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+package com.example.Backend.security;
+
+import com.example.Backend.security.jwt.JwtRequestFilter;
+import com.example.Backend.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UsuarioService authenticationService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authenticationService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login","/restaurante","/usuario/registrar").permitAll()
+                .antMatchers("/usuario/detalle").hasAnyAuthority("USER","ADMIN")
+                .antMatchers("/usuario", "/usuario/{id}").hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+}
