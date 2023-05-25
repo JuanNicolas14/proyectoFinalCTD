727ef7e5ab229c7ca0c8941e56e6461164b6fadc
CesarMauricioRojas <mauricio2328cr@gmail.com>
2023-05-25 17:59:01 -0500

    Historia de usuario 10, cambios en backend.

--- Backend/src/main/java/com/example/Backend/repository/UsuarioRolRepository.java
+++ Backend/src/main/java/com/example/Backend/repository/UsuarioRolRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuarioRolRepository extends JpaRepository<UsuarioRol,Long> {
+    //Buscar usuario por rol
     Optional<UsuarioRol> findByRol(String rol);
 }
