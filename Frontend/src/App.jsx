f5bf5c5e2b2ef985edb014f4018c8fe02ef83752
LeoPerezCTD <navarroandres77223@gmail.com>
2023-06-12 18:34:14 -0500

    historias 28 y 29 marcar y listar favoritos

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -17,33 +17,44 @@ import AuthContextProvider from './utils/AuthContext'
 import FilterContextProvider from './utils/FilterContext'
 import ListaPorFiltros from './Vistas/ListaPorFiltros/ListaPorFiltros'
 import ValidarUsuario from './Vistas/ValidarUsuario/ValidarUsuario'
+import ListaFavoritos from "./Vistas/ListaFavoritos/ListaFavoritos";
 
 function App() {
   return (
     <AuthContextProvider>
       <FilterContextProvider>
-        <Header/>
+        <Header />
         <Routes>
-          <Route path='/' element={<Navigate to='/home' replace/>}/>
-          <Route path='/home' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/restaurantes' element={<Listado/>} />
-          <Route path='/detalle/:id' element={<Detalle/>}/>
-          <Route path='/restaurantes' element={<Listado/>}/>
-          <Route path='/restaurantes/:plan' element={<ListaPorPlan/>}/>
-          <Route path='/administracion' element={<Administracion/>}/>
-          <Route path='/administracion/agregarProducto' element={<AgregarProducto/>}/>
-          <Route path='/administracion/eliminarProducto' element={<EliminarProducto/>}/>
-          <Route path='/usuario/detalle' element={<DetalleUser/>}/>
-          <Route path='/usuario/registrar' element={<RegistrarUsuario/>}/>
-          <Route path='/usuario/validar/:id' element={<ValidarUsuario/>}/>
-          <Route path='/plan/registrar' element={<AgregarPlan/>}/>
-          <Route path='/restaurantes/ciudad-y-plan' element={<ListaPorFiltros/>}/>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/restaurantes" element={<Listado />} />
+          <Route path="/detalle/:id" element={<Detalle />} />
+          <Route path="/restaurantes" element={<Listado />} />
+          <Route path="/restaurantes/:plan" element={<ListaPorPlan />} />
+          <Route path="/administracion" element={<Administracion />} />
+          <Route
+            path="/administracion/agregarProducto"
+            element={<AgregarProducto />}
+          />
+          <Route
+            path="/administracion/eliminarProducto"
+            element={<EliminarProducto />}
+          />
+          <Route path="/usuario/detalle" element={<DetalleUser />} />
+          <Route path="/usuario/registrar" element={<RegistrarUsuario />} />
+          <Route path="/usuario/validar/:id" element={<ValidarUsuario />} />
+          <Route path="/plan/registrar" element={<AgregarPlan />} />
+          <Route
+            path="/restaurantes/ciudad-y-plan"
+            element={<ListaPorFiltros />}
+          />
+          <Route path="/favoritos/:id" element={<ListaFavoritos />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </FilterContextProvider>
     </AuthContextProvider>
-  )
+  );
 }
 
 export default App
---
+++ Frontend/src/Vistas/ListaFavoritos/ListaFavoritos.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../utils/AuthContext";
+import baseUrl from "../../utils/baseUrl.json";
+import { FaTrash } from "react-icons/fa";
+import "./listaFavoritos.css";
+import Swal from "sweetalert2";
+
+const ListaFavoritos = () => {
+  const { user, token } = useContext(AuthContext);
+  const [listaFavoritos, setListaFavoritos] = useState([]);
+
+  const url = baseUrl.url + "/favoritos";
+
+  useEffect(() => {
+    obtenerProductosFavoritos();
+  }, []);
+
+  const obtenerProductosFavoritos = async () => {
+    try {
+      const response = await fetch(`${url}/${user.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setListaFavoritos(data);
+      }
+    } catch (error) {
+      console.error("Error al obtener los productos favoritos:", error);
+    }
+  };
+
+  const eliminarRestaurante = async (usuarioId, restauranteId) => {
+    try {
+      const response = await fetch(`${url}/${usuarioId}/${restauranteId}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        // Eliminar el restaurante de la lista en el estado
+        const nuevaLista = listaFavoritos.filter(
+          (restaurante) => restaurante.id !== restauranteId
+        );
+        setListaFavoritos(nuevaLista);
+        // Mostrar mensaje de éxito
+        Swal.fire({
+          icon: "success",
+          title: "Restaurante eliminado",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    } catch (error) {
+      console.error("Error al eliminar el restaurante:", error);
+    }
+  };
+
+  return (
+    <div className="restaurantes-favoritos">
+      <h2>Mis Restaurantes Favoritos</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Nombre</th>
+            <th>Ciudad</th>
+            <th>Plan</th>
+            <th>Precio</th>
+            <th>Puntuación</th>
+            <th>Eliminar</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaFavoritos.map((restaurante, index) => (
+            <tr key={restaurante.id}>
+              <td>{index + 1}</td>
+              <td>{restaurante.nombre}</td>
+              <td>{restaurante.domicilio.ciudad.nombreCiudad}</td>
+              <td>{restaurante.plan.nombre}</td>
+              <td>{restaurante.precio}</td>
+              <td>{restaurante.puntuacionPromedio}</td>
+              <td
+                className="fa-trash"
+                onClick={() => eliminarRestaurante(user.id, restaurante.id)}
+              >
+                <FaTrash />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ListaFavoritos;
