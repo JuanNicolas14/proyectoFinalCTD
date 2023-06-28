9d876670c3afd9b91a93e9589062085681fb3381
CesarMauricioRojas <mauricio2328cr@gmail.com>
2023-06-28 11:56:30 -0500

    Incorporación Spinner a la hora de ver comentarios

---
+++ Frontend/src/Componentes/SpinnerGrande/SpinnerGrande.jsx
@@ -0,0 +1,11 @@
+import "./spinnerGrande.css";
+
+export default function LoadingSpinner() {
+  return (
+    <div className="spinner-grande-container">
+      <div className="loading-grande-spinner">
+      </div>
+      <p>Cargando...</p>
+    </div>
+  );
+}
